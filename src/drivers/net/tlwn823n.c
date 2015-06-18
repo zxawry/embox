bbc286e68778415b4c7d9fcd1ffa42b26c8092a5
Denis Deryugin <deryugin.denis@gmail.com>
2015-06-18 16:06:03 +0300

    wn823n: Work on firmware handle
    rtl_write/read are stubs now, but the rest of code should be fine

--- src/drivers/net/tlwn823n.c
+++ src/drivers/net/tlwn823n.c
@@ -8,6 +8,7 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <stdio.h>
 #include <string.h>
 
 #include <drivers/usb/usb_driver.h>
@@ -36,7 +37,44 @@ struct wn823n_priv {
 	struct usb_dev *usbdev;
 	char *data;
 	char *pdata;
+	char *fw; /* Firmware */
+	size_t fw_len;
 };
+
+#define REG_MACID	0x0610
+static int rtl_read_byte(int addr) {
+	/* Stub */
+	return 0xff;
+}
+
+static int rtl_write_byte(int addr, char *data) {
+	/* Stub */
+	return 0;
+}
+
+#define FW_NAME       "firmware/rtl8192cufw.bin"
+#define FW_MAX_LEN    0x4000
+#define FW_START_ADDR 0x1000
+
+static int wn823n_load_firmware(struct wn823n_priv *priv) {
+	FILE *fs_fw;
+	int i;
+	assert(priv);
+
+	if (NULL == (fs_fw = fopen(FW_NAME, "r")))
+		return -EINVAL;
+
+	if ((priv->fw_len = fread(priv->fw, FW_MAX_LEN, 1, fs_fw)) < 0) {
+		return priv->fw_len;
+	}
+
+	for (i = 0; i < priv->fw_len; i++) {
+		if (rtl_write_byte(FW_START_ADDR + i, priv->fw + i))
+			return -1;
+	}
+
+	return 0;
+}
 
 static int wn823n_probe(struct usb_driver *drv, struct usb_dev *dev, void **data) {
 	struct net_device *nic;
@@ -54,6 +92,9 @@ static int wn823n_probe(struct usb_driver *drv, struct usb_dev *dev, void **data
 
 	nic->drv_ops = &wn823n_drv_ops;
 	nic_priv = netdev_priv(nic, struct wn823n_priv);
+	nic_priv->fw = sysmalloc(FW_MAX_LEN);
+	wn823n_load_firmware(nic_priv);
+
 	nic_priv->data = nic_priv->pdata = sysmalloc(ETH_FRAME_LEN);
 	if (!nic_priv->data) {
 		etherdev_free(nic);
@@ -78,12 +119,6 @@ static void wn823n_disconnect(struct usb_dev *dev, void *data) {
 static int wn823n_xmit(struct net_device *dev, struct sk_buff *skb) {
 	printk("wn823n xmit\n");
 	return 0;
-}
-
-#define REG_MACID	0x0610
-static int rtl_read_byte(int addr) {
-	/* Stub */
-	return 0xff;
 }
 
 static int wn823n_get_macaddr(struct net_device *dev, void *buff) {
