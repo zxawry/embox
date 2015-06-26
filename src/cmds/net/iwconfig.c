6e67a532d31b68dc191ce9bb79b7c7b37bdc69c1
Denis Deryugin <deryugin.denis@gmail.com>
2015-06-26 12:51:55 +0300

    iwconfig: Add my-file and source file

---
+++ src/cmds/net/iwconfig.c
@@ -0,0 +1,14 @@
+/**
+ * @file iwconfig.c
+ * @brief Configure wireless connections
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.0.1
+ * @date 2015-06-26
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Not implemented yet!\n");
+	return 0;
+}
