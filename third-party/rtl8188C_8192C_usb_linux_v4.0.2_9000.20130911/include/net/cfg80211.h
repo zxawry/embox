74611551a2598505c69650cb0aa381f6bb3dc1a8
Denis Deryugin <deryugin.denis@gmail.com>
2015-07-02 14:38:08 +0300

    Adding empty headers

---
+++ third-party/rtl8188C_8192C_usb_linux_v4.0.2_9000.20130911/include/net/cfg80211.h
@@ -0,0 +1,14 @@
+/**
+ * @file cfg80211.h
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.0.1
+ * @date 2015-07-02
+ */
+
+#ifndef NET_CFG80211
+#define NET_CFG80211
+
+/* Stub */
+
+#endif /* NET_CFG80211 */
---
+++ third-party/rtl8188C_8192C_usb_linux_v4.0.2_9000.20130911/include/net/ieee80211_radiotap.h
@@ -0,0 +1,12 @@
+/**
+ * @file ieee80211_radiotap.h
+ * @brief
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.0.1
+ * @date 2015-07-02
+ */
+
+#ifndef NET_IEEE80211_RADIOTAP
+#define NET_IEEE80211_RADIOTAP
+/* Stub */
+#endif /* NET_IEEE80211_RADIOTAP */
