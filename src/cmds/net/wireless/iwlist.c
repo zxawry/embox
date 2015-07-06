ca2e97c9faec0ae5ef2d9c2686525d9f8299ef4b
Denis Deryugin <deryugin.denis@gmail.com>
2015-07-06 15:21:05 +0300

    iwlist initial implementation
    Create c- and my- files

---
+++ src/cmds/net/wireless/iwlist.c
@@ -0,0 +1,14 @@
+/**
+ * @file iwlist.c
+ * @brief List wireless networks
+ * @author Denis Deryugin <deryugin.denis@gmail.com>
+ * @version 0.0.1
+ * @date 2015-07-06
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Not implemented yet.\n");
+	return 0;
+}
