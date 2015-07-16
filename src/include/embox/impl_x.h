0195d79cdc3e65ad1399788d51963e017e85cabc
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-07-16 20:08:45 +0300

    hack: Remove impl_x macro from cflags

---
+++ src/include/embox/impl_x.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief
+ *
+ * @author  Anton Kozlov
+ * @date    15.07.2015
+ */
+
+#ifndef EMBOX_IMPL_X_H_
+#define EMBOX_IMPL_X_H_
+
+#define __impl_x(path) <../path>
+
+#endif /* EMBOX_IMPL_X_H_ */
