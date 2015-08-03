b2c7226f341c48c04ed12378d58d79e60098a9e1
Alex Kalmuk <alexkalmuk@gmail.com>
2015-08-03 19:25:59 +0300

    rtlwifi: CONFIG_PREALLOC_RECV_SKB set to 0

--- third-party/rtl8188C_8192C_usb_linux_v4.0.2_9000.20130911/include/autoconf.h
+++ third-party/rtl8188C_8192C_usb_linux_v4.0.2_9000.20130911/include/autoconf.h
@@ -206,7 +206,7 @@
 	#define CONFIG_USB_RX_AGGREGATION	1
 #endif
 
-#define CONFIG_PREALLOC_RECV_SKB	1
+#define CONFIG_PREALLOC_RECV_SKB	0
 //#define CONFIG_REDUCE_USB_TX_INT	1	// Trade-off: Improve performance, but may cause TX URBs blocked by USB Host/Bus driver on few platforms.
 //#define CONFIG_EASY_REPLACEMENT	1
 
